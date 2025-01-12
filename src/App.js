c30fbd101a672745eefeac7df63c537e362f3983
Nardelys15c <136908202+Nardelys15c@users.noreply.github.com>
2025-01-12 12:11:39 -0500

    Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import logo from './booksletters.png';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo}  className="App-logo" alt="logo" />
-       
-        
-      </header>
+          </header>
+          <main>
+            <Dictionary />
+          </main>
+     
+    <footer className="App-footer"><small>
+      Coded by Nardelys Caraballo and hosted by <a href='https://github.com/Nardelys15c/project-dictionary.git'>Github</a></small>
+    </footer>
+    </div>
     </div>
   );
 }
