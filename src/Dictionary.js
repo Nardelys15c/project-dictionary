c1a547c0bb66db09556514a60213634ea81df19f
Nardelys15c <136908202+Nardelys15c@users.noreply.github.com>
2025-02-20 17:10:51 -0500

    API changes and css editing

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,33 @@
 import React, {useState} from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import './Dictionary.css';
 
 export default function Dictionary(props){
     let [keyword, setKeyword]=useState(props.defaultKeyword);
     let [results, setResults]= useState(null);
     let [loaded, setLoaded]=useState(false);
+    let [photos, setPhotos]=useState(null);
 
-function handleResponse(response){
+function handleDictionaryResponse(response){
         setResults(response.data[0]);
 }
+function handlePexelsResponse(response){
+    setPhotos(response.data.photos);
+}
+
 function search (){
     let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+
+    let pexelsApiKey= "7bf4e319208b1e35aafo3ce425b6c3at";
+    let pexelsApiUrl=`https://api.shecodes.io/images/v1/search?query=${keyword}&key=7bf4e319208b1e35aafo3ce425b6c3at`
+    axios.get(pexelsApiUrl, { headers: {"Authorization" : `Bearer ${pexelsApiKey}`}}).then(handlePexelsResponse);
+
 }
+
     
 function handleSubmit(event){
    event.preventDefault();
@@ -42,6 +55,7 @@ if (loaded){
         </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos}/>
       </div>   
       );
     }  else{
